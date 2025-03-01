8027c4f31e58ec22be5a7cb9f834dfc32a85eb10
Daniel Frey <daniel.frey@xmatrix.ch>
2025-03-01 14:44:05 +0100

    fix: upgrade to React Router v7 and fix browser console warnings

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,33 +5,23 @@ import OrderList from './components/OrderList';
 import OrderDetails from './components/OrderDetails';
 import ProtectedRoute from './components/ProtectedRoute';
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      children: [
-        {
-          path: '/',
-          element: <Login />
-        },
-        {
-          path: '/orders',
-          element: <ProtectedRoute><OrderList /></ProtectedRoute>
-        },
-        {
-          path: '/orders/:orderId',
-          element: <ProtectedRoute><OrderDetails /></ProtectedRoute>
-        }
-      ]
-    }
-  ],
+export const router = createBrowserRouter([
   {
-    basename: undefined,
-    future: {
-      v7_normalizeFormMethod: true,
-      v7_relativeSplatPath: true,
-      v7_startTransition: true
-    }
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Login />
+      },
+      {
+        path: 'orders',
+        element: <ProtectedRoute><OrderList /></ProtectedRoute>
+      },
+      {
+        path: 'orders/:orderId',
+        element: <ProtectedRoute><OrderDetails /></ProtectedRoute>
+      }
+    ]
   }
-);
+]);
