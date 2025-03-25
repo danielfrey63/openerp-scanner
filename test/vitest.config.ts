afa4242d6fc6f9da7346a796ed824ce749d8f29b
Daniel Frey <daniel.frey@xmatrix.ch>
2025-03-25 08:14:20 +0100

    refactor: removed original test monolith

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -20,23 +20,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'lcov'],
-      exclude: [
-        '**/node_modules/**', 
-        '**/dist/**', 
-        '**/.idea/**', 
-        '**/.git/**', 
-        '**/.cache/**', 
-        '**/temp/**',
-        '**/setup.ts',
-        '**/*.config.ts',
-        '**/src/App.tsx',
-        '**/src/env.d.ts',
-        '**/src/main.tsx',
-        '**/src/routes.tsx',
-        '**/src/types.d.ts',
-        '**/src/context/OpenERPContext.tsx',
-        '**/src/types/components.d.ts'
-      ],
+      exclude: ['**/node_modules/**', '**/dist/**', '**/.idea/**', '**/.git/**', '**/.cache/**', '**/temp/**'],
       include: [
         'src/components/OrderDetails.tsx',
         'test/components/OrderDetails/**/*.test.{ts,tsx}'
